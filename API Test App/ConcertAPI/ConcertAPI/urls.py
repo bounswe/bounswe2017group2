b810cfe994c6fde36cad7875b53b09d8b2b2f218
Kemal Berk Kocabagli <kberkkocabagli@gmail.com>
2017-04-25 11:32:12 +0300

    Fixed some bugs in concert/urls.py, concert/views.py, ConcertAPI/urls.py

--- API Test App/ConcertAPI/ConcertAPI/urls.py
+++ API Test App/ConcertAPI/ConcertAPI/urls.py
@@ -4,4 +4,6 @@ from concert import views
 urlpatterns = [
                url(r'^concert/$', views.concert_list),
                url(r'^concert/(?P<pk>[0-9]+)/$', views.concert_detail),
+               url(r'^user/$', views.user_list),
+               url(r'^user/(?P<pk>[0-9]+)/$', views.user_detail),
                ]
