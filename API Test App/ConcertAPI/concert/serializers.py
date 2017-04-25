5ff86d61fad54a60811e001775628489074e515a
bariskurt <bariskurt@gmail.com>
2017-04-25 03:31:03 +0300

    Created User Model and User serializer

--- API Test App/ConcertAPI/concert/serializers.py
+++ API Test App/ConcertAPI/concert/serializers.py
@@ -33,3 +33,33 @@ class ConcertSerializer(serializers.Serializer):
         instance.maxprice = validated_data.get('maxprice', instance.maxprice)
         instance.save()
         return instance
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    
+    # Name of the User, REQUIRED FIELD
+    name = serializers.CharField(required=True, max_length=20)
+    # Email of the User, REQUIRED FIELD
+    email = serializers.CharField(required=True, max_length=30)
+    # Password of the User, REQUIRED FIELD
+    password = serializers.CharField(required=True)
+    # Age price of User tickets, OPTIONAL FIELD
+    age = serializers.IntegerField(default=0)
+    
+    def create(self, validated_data):
+        """
+            Create and return a new `User` instance, given the validated data.
+            """
+        return User.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+            Update and return an existing `User` instance, given the validated data.
+            """
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        instance.age = validated_data.get('age', instance.age)
+        instance.save()
+        return instance
