1240c3ac00be3b4a5c04ca6989766cae090daee8
HalukKaraevli <hakaraevli@gmail.com>
2017-05-08 23:01:43 +0300

    Created Get Unit Tests for Concert
    I added unit tests for;
    - get concert by ID
    - get all concerts

--- API Test App/ConcertAPI/concert/tests.py
+++ API Test App/ConcertAPI/concert/tests.py
@@ -16,7 +16,7 @@ class TestsForConcerts(APITestCase):
         Concert.objects.create(artist="Bulent Ortacgil", location ="BogaziciUniTasoda", date="2017-05-21")
         sleep(1)
                 
-    #Testcase 1
+    #Testcase 1:
     def test_update_concert(self):
 
         url = '/concert/1/'
@@ -24,6 +24,18 @@ class TestsForConcerts(APITestCase):
         response = self.client.put(url, data, format='json')
         self.assertEqual(json.loads(response.content), {'id': 1, 'artist': 'Sebnem Ferah', 'date':'2017-06-20', 'location':'BogaziciUniTasoda', 'minprice': 0, 'maxprice': 0})
 
+    #Testcase 2:
+    def test_get_all_concerts(self):
+        url = '/concert/'
+        response = self.client.get(url)
+        self.assertEqual(json.loads(response.content), [{'id': 1, 'artist': 'Duman', 'location': 'BogaziciUniTasoda', 'date': '2017-05-20', 'minprice': 0, 'maxprice': 0},{'id': 2, 'artist': 'Bulent Ortacgil', 'location': 'BogaziciUniTasoda', 'date': '2017-05-21', 'minprice': 0, 'maxprice': 0}])
+    
+    #Testcase 3:
+    def test_get_concert_via_id(self):
+        url = '/concert/1/'
+        response = self.client.get(url)
+        self.assertEqual(json.loads(response.content), {'id': 1, 'artist': 'Duman', 'location': 'BogaziciUniTasoda', 'date': '2017-05-20', 'minprice': 0, 'maxprice': 0})
+ 
 
 if __name__ == '__main__':
     unittest.main()
