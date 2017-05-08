a301ddd687a7804fcd12fb743c520a57fb6e06b4
Elif Güler <elgu96@gmail.com>
2017-05-08 23:49:18 +0300

    Small changes in comments

--- API Test App/ConcertAPI/concert/tests.py
+++ API Test App/ConcertAPI/concert/tests.py
@@ -16,7 +16,7 @@ class TestsForConcerts(APITestCase):
         Concert.objects.create(artist="Bulent Ortacgil", location ="BogaziciUniTasoda", date="2017-05-21")
         sleep(1)
                 
-    #Testcase 1:
+    #Testcase Concert 1:
     def test_update_concert(self):
 
         url = '/concert/1/'
@@ -24,7 +24,7 @@ class TestsForConcerts(APITestCase):
         response = self.client.put(url, data, format='json')
         self.assertEqual(json.loads(response.content), {'id': 1, 'artist': 'Sebnem Ferah', 'date':'2017-06-20', 'location':'BogaziciUniTasoda', 'minprice': 0, 'maxprice': 0})
 
-    #Testcase 2
+    #Testcase Concert 2:
     def test_create_concert(self):
         url = '/concert/'
         data = {'artist': 'Sezen Aksu', 'date':'2017-05-20', 'location':'BogaziciUniTasoda'}
@@ -32,19 +32,19 @@ class TestsForConcerts(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(json.loads(response.content), {'id': 3, 'artist': 'Sezen Aksu', 'location': 'BogaziciUniTasoda', 'date': '2017-05-20', 'minprice': 0, 'maxprice': 0})
 
-    #Testcase 3:
+    #Testcase Concert 3:
     def test_get_all_concerts(self):
         url = '/concert/'
         response = self.client.get(url)
         self.assertEqual(json.loads(response.content), [{'id': 1, 'artist': 'Duman', 'location': 'BogaziciUniTasoda', 'date': '2017-05-20', 'minprice': 0, 'maxprice': 0},{'id': 2, 'artist': 'Bulent Ortacgil', 'location': 'BogaziciUniTasoda', 'date': '2017-05-21', 'minprice': 0, 'maxprice': 0}])
     
-    #Testcase 4:
+    #Testcase Concert 4:
     def test_get_concert_via_id(self):
         url = '/concert/1/'
         response = self.client.get(url)
         self.assertEqual(json.loads(response.content), {'id': 1, 'artist': 'Duman', 'location': 'BogaziciUniTasoda', 'date': '2017-05-20', 'minprice': 0, 'maxprice': 0})
     
-    #Testcase 4:
+    #Testcase Concert 5:
     def test_delete_concert(self):
         url = '/concert/1/'
         response = self.client.delete(url)
