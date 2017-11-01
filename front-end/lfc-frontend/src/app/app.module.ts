6f50d16219334ab8488a1ba8455ca229cac0966f
buraksofu <buraksofu77@gmail.com>
2017-11-01 14:15:22 +0300

    Folders rearrenged and login-form module created

--- front-end/lfc-frontend/src/app/app.module.ts
+++ front-end/lfc-frontend/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CreateConcertComponent } from './create-concert.component';
+import { CreateConcertComponent } from './create-concert/create-concert.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {Headers, RequestOptions} from '@angular/http'; 
+import {Headers, RequestOptions} from '@angular/http';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component'; 
+
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
-    CreateConcertComponent
+    CreateConcertComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -14,6 +23,6 @@ import {Headers, RequestOptions} from '@angular/http';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [CreateConcertComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
