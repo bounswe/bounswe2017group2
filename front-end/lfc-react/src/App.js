8edaddc06b8a0fa71da9d7529c49c2ef29c308d7
buraksofu <buraksofu77@gmail.com>
2017-11-17 00:19:58 +0300

    Eslint and dependencies are set

--- front-end/lfc-react/src/App.js
+++ front-end/lfc-react/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
-//import SignupPage from "./components/pages/SignupPage";
+// import SignupPage from "./components/pages/SignupPage";
 
 const App = () => (
   <div className="ui container">
