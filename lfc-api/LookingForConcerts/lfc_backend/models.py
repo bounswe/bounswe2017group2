cd75f237eabc453390ad6fab8e95552163440fcb
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2017-11-26 12:21:50 +0300

    Deleted unnecessary function: create_auth_token

--- lfc-api/LookingForConcerts/lfc_backend/models.py
+++ lfc-api/LookingForConcerts/lfc_backend/models.py
@@ -8,19 +8,13 @@ from django.utils.translation import ugettext_lazy as _
 from .managers import UserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 from django.conf import settings
 
 import datetime
 
-# This code is triggered whenever a new user has been created and saved to the database
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
 
 # Models in our Database.
+
 class UserImage(models.Model):
     image = models.FileField(upload_to='user/')
 
