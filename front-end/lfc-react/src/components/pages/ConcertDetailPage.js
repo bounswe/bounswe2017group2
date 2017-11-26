2422bf46260af8420083224e3114eb08bfb4659d
halilozanakgul <hozanakgul@gmail.com>
2017-11-26 16:57:48 +0300

    Removed some parts
    Removed some parts to be able to merge in smaller parts into the master branch.

--- front-end/lfc-react/src/components/pages/ConcertDetailPage.js
+++ front-end/lfc-react/src/components/pages/ConcertDetailPage.js
@@ -19,65 +19,6 @@ var userID;
 const theToken = localStorage.getItem("lfcJWT");
 //lat: 41.015, lng: 28.979
 
-class CommentBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({
-            value: event.target.value,
-        });
-    }
-
-    handleSubmit(event) {
-        console.log("Bearer " + theToken);
-        console.log(this.state.value);
-        axios.post('http://34.210.127.92:8000/concert/' + this.props.concertID + '/newcomment/', {
-            'content': this.state.value,
-        }, {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + theToken
-            }).then(response => {
-                window.location.reload();
-            }, error => {
-                console.log("refresh");
-            });
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <form className="ui form" id="commentBox" onSubmit={this.handleSubmit}>
-                <div className=" field">
-                    <textarea rows="5" value={this.state.value} onChange={this.handleChange}></textarea>
-                </div>
-                <div className="ui icon buttons">
-                    <button className="ui button">
-                        <i className="photo icon"></i>
-                    </button>
-                    <button className="ui button">
-                        <i className="sound icon"></i>
-                    </button>
-                    <button className="ui button">
-                        <i className="film icon"></i>
-                    </button>
-                </div>
-                <button className="ui right floated labeled icon button" type="Submit">
-                    <i className="icon edit"></i>
-                    Comment
-                </button>
-            </form>
-        );
-    }
-}
-
 class Concert extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -142,7 +83,7 @@ class Concert extends React.PureComponent {
                     console.log("refresh");
                 });
         }
-        else{
+        else {
             axios.post('http://34.210.127.92:8000/concert/' + this.props.match.params.concertID + '/unsubscribe/', {
             }, {
                     'Content-Type': 'application/json',
@@ -156,53 +97,8 @@ class Concert extends React.PureComponent {
     }
 
     render() {
-        userID=decode(localStorage.lfcJWT).user_id;
+        userID = decode(localStorage.lfcJWT).user_id;
         console.log(userID);
-        var visibleMessage = "";
-        var invisibleMessage = "";
-        var dateOfConcert = new Date(this.state.concert.date_time);
-        if (dateOfConcert > Date.now()) {
-            visibleMessage = "Attending";
-            invisibleMessage = "Not Attending";
-        }
-        else {
-            visibleMessage = "Attended";
-            invisibleMessage = "Didn't Attend";
-        }
-        if (this.state.concert.attendees.indexOf(userID) == -1) {
-            var temp;
-            temp = visibleMessage;
-            visibleMessage = invisibleMessage;
-            invisibleMessage = temp;
-        }
-        let commentBox = null;
-        if (theToken) {
-            commentBox = <CommentBox concertID={this.props.match.params.concertID} />;
-        }
-        var coor = this.state.concert.location.coordinates;
-        this.state.concert.location.lat = parseFloat(coor.substring(0, coor.indexOf(" ")), 10);
-        this.state.concert.location.lng = parseFloat(coor.substring(coor.indexOf(" ")), 10);
-        var ConcertLocationMap = compose(
-            withProps({
-                googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" + googleMapKey,
-                loadingElement: <div style={{ height: `100%` }} />,
-                containerElement: <div style={{ height: `300px` }} />,
-                mapElement: <div style={{ height: `100%` }} />,
-            }),
-            withScriptjs,
-            withGoogleMap,
-        )((props) =>
-            <GoogleMap
-                defaultZoom={12}
-                defaultCenter={{ lat: this.state.concert.location.lat, lng: this.state.concert.location.lng }}
-            >
-                <Marker position={{ lat: this.state.concert.location.lat, lng: this.state.concert.location.lng }} onClick={props.onMarkerClick}>
-                    {<InfoWindow>
-                        <h3><b>{this.state.concert.location.venue}</b></h3>
-                    </InfoWindow>}
-                </Marker>
-            </GoogleMap>
-            )
         return (
             <div className="ui grid segment start">
                 <div className="row">
@@ -219,23 +115,19 @@ class Concert extends React.PureComponent {
 
                 <div className="row">
                     <img className="ui image five wide column" height="300px" src={this.state.concert.artist.images[0].url} />
-                    <div className="eleven wide column" >
-                        <ConcertLocationMap
-                            isMarkerShown={true}
-                        />
-                    </div>
+
                 </div>
+
+
+
                 <div className="row">
                     <div className="sixteen wide column">
                         <p>{this.state.concert.description}</p>
-                        <button className="ui animated fade fluid button" onClick={this.handleFollowButton}>
-                            <div className="visible content"> {visibleMessage} </div>
-                            <div className="hidden content">
-                                Mark as {invisibleMessage}
-                            </div>
-                        </button>
+
+                        
                     </div>
                 </div>
+
                 <div className="row">
                     <div className="sixteen wide column">
                         <div className="ui comments">
@@ -260,7 +152,6 @@ class Concert extends React.PureComponent {
                                     </div>
                                 )
                             }
-                            {commentBox}
                         </div>
                     </div>
                 </div >
