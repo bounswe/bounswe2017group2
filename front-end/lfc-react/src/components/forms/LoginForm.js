ae402a4b2259c3a7fdcf9c8754637d4aaa8a1b48
halilozanakgul <hozanakgul@gmail.com>
2017-11-26 19:27:58 +0300

    Fixed inline-inLine error

--- front-end/lfc-react/src/components/forms/LoginForm.js
+++ front-end/lfc-react/src/components/forms/LoginForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Form, Button, Message } from "semantic-ui-react";
 // import Validator from "validator";
-import InlineError from "../messages/InLineError";
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends React.Component {
   state = {
