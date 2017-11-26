952b24c3f94fc0e1c49f335206ac483b65a7f2d9
halilozanakgul <hozanakgul@gmail.com>
2017-11-26 20:43:41 +0300

    Added comment box and follow button

--- front-end/lfc-react/src/components/pages/ConcertDetailPage.js
+++ front-end/lfc-react/src/components/pages/ConcertDetailPage.js
@@ -19,6 +19,66 @@ var userID;
 const theToken = localStorage.getItem("lfcJWT");
 //lat: 41.015, lng: 28.979
 
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value,
+        });
+    }
+
+    handleSubmit(event) {
+        console.log("Bearer " + theToken);
+        console.log(this.state.value);
+        axios.post('http://34.210.127.92:8000/concert/' + this.props.concertID + '/newcomment/', {
+            'content': this.state.value,
+        }, {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + theToken
+            }).then(response => {
+                window.location.reload();
+            }, error => {
+                console.log("refresh");
+            });
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className="ui form" id="commentBox" onSubmit={this.handleSubmit}>
+                <div className=" field">
+                    <textarea rows="5" value={this.state.value} onChange={this.handleChange}></textarea>
+                </div>
+                <div className="ui icon buttons">
+                    <button className="ui button">
+                        <i className="photo icon"></i>
+                    </button>
+                    <button className="ui button">
+                        <i className="sound icon"></i>
+                    </button>
+                    <button className="ui button">
+                        <i className="film icon"></i>
+                    </button>
+                </div>
+                <button className="ui right floated labeled icon button" type="Submit">
+                    <i className="icon edit"></i>
+                    Comment
+                </button>
+            </form>
+        );
+    }
+}
+
+
 class Concert extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -102,6 +162,28 @@ class Concert extends React.PureComponent {
         userID = decode(localStorage.lfcJWT).user_id;
         console.log(userID);
 
+        var visibleMessage = "";
+        var invisibleMessage = "";
+        var dateOfConcert = new Date(this.state.concert.date_time);
+        if (dateOfConcert > Date.now()) {
+            visibleMessage = "Attending";
+            invisibleMessage = "Not Attending";
+        }
+        else {
+            visibleMessage = "Attended";
+            invisibleMessage = "Didn't Attend";
+        }
+        if (this.state.concert.attendees.indexOf(userID) == -1) {
+            var temp;
+            temp = visibleMessage;
+            visibleMessage = invisibleMessage;
+            invisibleMessage = temp;
+        }
+        let commentBox = null;
+        if (theToken) {
+            commentBox = <CommentBox concertID={this.props.match.params.concertID} />;
+        }
+
 
         var coor = this.state.concert.location.coordinates;
         this.state.concert.location.lat = parseFloat(coor.substring(0, coor.indexOf(" ")), 10);
@@ -160,6 +242,12 @@ class Concert extends React.PureComponent {
                     <div className="sixteen wide column">
                         <p>{this.state.concert.description}</p>
 
+                        <button className="ui animated fade fluid button" onClick={this.handleFollowButton}>
+                            <div className="visible content"> {visibleMessage} </div>
+                            <div className="hidden content">
+                                Mark as {invisibleMessage}
+                            </div>
+                        </button>
 
                     </div>
                 </div>
@@ -188,6 +276,8 @@ class Concert extends React.PureComponent {
                                     </div>
                                 )
                             }
+
+                            {commentBox}
                         </div>
                     </div>
                 </div >
