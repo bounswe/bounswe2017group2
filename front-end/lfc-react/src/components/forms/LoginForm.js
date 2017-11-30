e68e5a8f32a17bf05b74186b61ace16159c8e4ca
buraksofu <buraksofu77@gmail.com>
2017-11-30 12:31:42 +0300

    Inline bug fixed

--- front-end/lfc-react/src/components/forms/LoginForm.js
+++ front-end/lfc-react/src/components/forms/LoginForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Form, Button, Message } from "semantic-ui-react";
-import InlineError from "../messages/InLineError";
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends React.Component {
   state = {
--- front-end/lfc-react/src/components/forms/SignupForm.js
+++ front-end/lfc-react/src/components/forms/SignupForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Form, Button, Message } from "semantic-ui-react";
 import { isEmail, isLength } from "validator";
-import InlineError from "..//messages/InLineError";
+import InlineError from "..//messages/InlineError";
 
 class SignupForm extends React.Component {
   state = {
