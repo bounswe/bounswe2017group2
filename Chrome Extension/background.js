db600ce3cdd86782d8c843095098385db8c597d2
halilozanakgul <hozanakgul@gmail.com>
2017-12-27 15:35:34 +0300

    Some improvements
    Now popups work when scrolled.
    Show/hide annotation button.

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,18 +1,30 @@
 var xClick = 0;
 var yClick = 0;
 var pageHref;
+var popupY;
+var isShow = false;
 
-var openAnnotationWindow = function(e, tab){
-    popupWindow = window.open("popup_window.html","",
-        "width=460, height=260, top="+yClick+", left="+xClick);
-    console.log("top="+yClick+", left="+xClick); 
+var openAnnotationWindow = function (e, tab) {
+    popupWindow = window.open("popup_window.html", "",
+        "width=460, height=260, top=" + popupY + ", left=" + xClick);
 }
 
-chrome.runtime.onMessage.addListener(function(msg, sender){
-    console.log(msg);
-    xClick = msg.x;
-    yClick = msg.y;
-    pageHref = msg.url;
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.greetings == "right click") {
+        xClick = msg.x;
+        yClick = msg.y;
+        pageHref = msg.url;
+        popupY = msg.popupWindowY;
+    }
+    if (msg.greetings == "get isShow"){
+        console.log("get isShow=" + isShow);
+        sendResponse({ "isShow": isShow });
+    }
+    if (msg.greetings == "set isShow"){
+        console.log(sender);
+        console.log("set isShow="+msg.isShow);
+        isShow = msg.isShow;
+    }
 });
 
 chrome.contextMenus.create({
@@ -23,8 +35,8 @@ chrome.contextMenus.create({
     onclick: openAnnotationWindow,
 });
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details){
-    chrome.tabs.executeScript(null, {file: "content.js"});
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+    chrome.tabs.executeScript(null, { file: "content.js" });
 });
 
 console.log("Started Background.js");
--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -1,47 +1,84 @@
 document.addEventListener('mouseup', function (mouse) {
     if (mouse.button == 2) {
-        var coor = { x: mouse.clientX, y: mouse.clientY, url: window.location.href };
-        chrome.runtime.sendMessage(coor);
+        var msg = {
+            greetings: "right click",
+            x: mouse.clientX,
+            y: mouse.clientY + window.pageYOffset,
+            popupWindowY: mouse.clientY,
+            url: window.location.href
+        };
+        chrome.runtime.sendMessage(msg);
     }
 })
 
-
-var annotations;
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", "http://34.210.127.92:8000/list_annotations/?url=" + encodeURIComponent(window.location.href));
-xhttp.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTE0MzQ0OTIwLCJqdGkiOiI4ODg0YzAyMzU3N2M0NjkwODYwNWJhYzkxZDEzZjc2ZiIsInVzZXJfaWQiOjZ9.AFQxuf_HdVV0dkkZMGNO-fD5Hsupisg1fM6V5e3Y-Us");
-xhttp.onload = function () {
-    annotations = JSON.parse(this.response);
-    while (1) {
+function showAnnotations() {
+    var annotations;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://34.210.127.92:8000/list_annotations/?url=" + encodeURIComponent(window.location.href));
+    xhttp.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTE0MzgwNTQzLCJqdGkiOiI1ZjlmZTBhMGUxNDY0MjMzYTZmNGIyN2U2MTljMmNlYyIsInVzZXJfaWQiOjZ9.9rrhOEWpkCO-FXwL6QOv0bp697-ffeB_dSKTDGBmk0I");
+    xhttp.onload = function () {
+        annotations = JSON.parse(this.response);
         var oldAnnos = document.getElementsByClassName("annoPoi");
-        if(oldAnnos.length==0) break;
-        document.body.removeChild(oldAnnos[0]);
+        console.log(oldAnnos);
+        for (anno = oldAnnos.length - 1; anno >= 0; anno--) {
+            document.body.removeChild(oldAnnos[anno]);
+        }
+        var annoPointer = new Array(annotations.length);
+        var annoX = new Array(annotations.length);
+        var annoY = new Array(annotations.length);
+        for (annoN in annotations) {
+            (function (annoN) {
+                var anno = annotations[annoN];
+                var coors = anno.target[0].target_id;
+                coors = coors.substring(coors.indexOf('#'));
+                annoX[annoN] = parseInt(coors.substring(4, coors.indexOf(',')));
+                annoY[annoN] = parseInt(coors.substring(coors.indexOf(',') + 1));
+                annoPointer[annoN] = document.createElement("div");
+                annoPointer[annoN].className += " annoPoi";
+                annoPointer[annoN].innerHTML = "<div class='ui empty circular label' style='background-color: #800000; position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'></div>";
+                annoPointer[annoN].onmouseenter = function () {
+                    var div = document.createElement("div");
+                    div.innerHTML = "<div class='ui raised segment' style='position:absolute; left:" + annoX[annoN] + "px; top:" + (annoY[annoN] + 15) + "px'><div class='content'><div class='description'>" + anno.body[0].value + "</div></div></div>"
+                    annoPointer[annoN].appendChild(div);
+                }
+                annoPointer[annoN].onmouseleave = function () {
+                    annoPointer[annoN].innerHTML = "<div class='ui empty circular label' style='background-color: #800000; position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'></div>";
+                }
+                document.body.appendChild(annoPointer[annoN]);
+            })(annoN);
+        }
     }
-    var annoPointer = new Array(annotations.length);
-    var annoX = new Array(annotations.length);
-    var annoY = new Array(annotations.length);
-    for (annoN in annotations) {
-        (function (annoN) {
-            var anno = annotations[annoN];
-            var coors = anno.target[0].target_id;
-            coors = coors.substring(coors.indexOf('#'));
-            annoX[annoN] = parseInt(coors.substring(4, coors.indexOf(',')));
-            annoY[annoN] = parseInt(coors.substring(coors.indexOf(',') + 1));
-            annoPointer[annoN] = document.createElement("div");
-            annoPointer[annoN].className += " annoPoi";
-            annoPointer[annoN].innerHTML = "<div class='ui red empty circular label' style='position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'></div>";
-            annoPointer[annoN].onmouseenter = function () {
-                var div = document.createElement("div");
-                div.innerHTML = "<div class='ui card' style='position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'><div class='content'><div class='description'>" + anno.body[0].value + "</div></div></div>"
-                annoPointer[annoN].appendChild(div);
-            }
-            annoPointer[annoN].onmouseleave = function () {
-                annoPointer[annoN].innerHTML = "<div class='ui red empty circular label' style='position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'></div>";
-            }
-            document.body.appendChild(annoPointer[annoN]);
-        })(annoN);
+    xhttp.send();
+}
+
+function hideAnnotations() {
+    var oldAnnos = document.getElementsByClassName("annoPoi");
+    console.log(oldAnnos);
+    for (anno = oldAnnos.length - 1; anno >= 0; anno--) {
+        document.body.removeChild(oldAnnos[anno]);
     }
 }
-xhttp.send();
+
+var isShow;
+
+chrome.runtime.sendMessage({ greetings: "get isShow" }, function (response) {
+    isShow = response.isShow;
+    if (isShow) showAnnotations();
+    else hideAnnotations();
+})
+
+
+chrome.runtime.onMessage.addListener(
+    function (msg, sender) {
+        if (msg.greetings == "show annos") {
+            showAnnotations();
+            chrome.runtime.sendMessage({greetings: "set isShow", "isShow": true});
+        }
+        if (msg.greetings == "hide annos") {
+            hideAnnotations();
+            chrome.runtime.sendMessage({greetings: "set isShow", "isShow": false});
+        }
+    }
+)
 
 console.log("Start Page Script");
