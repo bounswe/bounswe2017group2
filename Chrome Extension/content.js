bb0957ca84ebf970f86785952b1a09a10c525d5b
halilozanakgul <hozanakgul@gmail.com>
2017-12-27 04:36:23 +0300

    Annotations on point
    Now annotations can be sent and views on coordinates

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -1,33 +1,42 @@
 document.addEventListener('mouseup', function (mouse) {
     if (mouse.button == 2) {
-        var coor = { x: mouse.clientX, y: mouse.clientY, url:window.location.href };
+        var coor = { x: mouse.clientX, y: mouse.clientY, url: window.location.href };
         chrome.runtime.sendMessage(coor);
     }
-}) 
+})
 
 
-var annoText;
+var annotations;
 var xhttp = new XMLHttpRequest();
-xhttp.open("GET", "http://34.210.127.92:8000/list_annotations/?url="+encodeURIComponent(window.location.href));
-xhttp.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTE0MzM1OTU5LCJqdGkiOiJmMzljYzE1NWE2YTA0ODUxOWFhNzljZThiMjI0NGJhOSIsInVzZXJfaWQiOjZ9.U7riDfHeCdt030Mn2LWOF7UYWZ2K70YSKidOCUZyzcw");
-xhttp.onload = function(){
-    annoText=JSON.parse(this.response)[0].body[0].value;
+xhttp.open("GET", "http://34.210.127.92:8000/list_annotations/?url=" + encodeURIComponent(window.location.href));
+xhttp.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTE0MzM4NzYzLCJqdGkiOiI5YWQyODFlOGU5OWQ0ZjI4YWNhZTM5YTE3MDNkYzNlOCIsInVzZXJfaWQiOjZ9.wuvsNSTHGAe3qH8gIdLRrWyYfqLD418nCEYgMk5Y1fw");
+xhttp.onload = function () {
+    annotations = JSON.parse(this.response);
+    var annoPointer = new Array(annotations.length);
+    var annoX = new Array(annotations.length);
+    var annoY = new Array(annotations.length);
+    for (annoN in annotations) {
+        (function (annoN) {
+            var anno = annotations[annoN];
+            console.log(anno);
+            var coors = anno.target[0].target_id;
+            coors = coors.substring(coors.indexOf('#'));
+            annoX[annoN] = parseInt(coors.substring(4, coors.indexOf(',')));
+            annoY[annoN] = parseInt(coors.substring(coors.indexOf(',') + 1));
+            annoPointer[annoN] = document.createElement("div");
+            console.log(annoX[annoN] + ' ' + annoY[annoN] + ' ' + anno.body[0].value);
+            annoPointer[annoN].innerHTML = "<div class='ui red empty circular label' style='position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'></div>";
+            annoPointer[annoN].onmouseenter = function () {
+                var div = document.createElement("div");
+                div.innerHTML = "<div class='ui card' style='position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'><div class='content'><div class='description'>" + anno.body[0].value + "</div></div></div>"
+                annoPointer[annoN].appendChild(div);
+            }
+            annoPointer[annoN].onmouseleave = function () {
+                annoPointer[annoN].innerHTML = "<div class='ui red empty circular label' style='position:absolute; left:" + annoX[annoN] + "px; top:" + annoY[annoN] + "px'></div>";
+            }
+            document.body.appendChild(annoPointer[annoN]);
+        })(annoN);
+    }
 }
 xhttp.send();
-
-
-var popla=document.createElement("div");
-popla.innerHTML="<div class='ui red empty circular label' style='position:absolute; left:500px; top:100px'></div>";
-popla.onmouseenter = function(){
-    var div=document.createElement("div");
-    div.innerHTML="<div class='ui card' style='position:absolute; left:495px; top:120px'><div class='content'><div class='description'>"+annoText+"</div></div></div>";
-    popla.appendChild(div);
-    console.log("oz");
-}
-popla.onmouseleave = function(){
-    popla.innerHTML="<div class='ui red empty circular label' style='position:absolute; left:500px; top:100px'></div>";
-    console.log("an");
-}
-
-document.body.appendChild(popla);
 console.log("Start Page Script");
