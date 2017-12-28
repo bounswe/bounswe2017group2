07898cbe635ebab010b206bb380c730dc3f4003c
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2017-12-28 16:34:53 +0300

    Backend/general enhancement (#347)
    * redirect to /home/ if no matches found
    
    * quick fix
    
    * quick fix
    
    * fix in advanced search

--- lfc-api/LookingForConcerts/lfc_backend/views.py
+++ lfc-api/LookingForConcerts/lfc_backend/views.py
@@ -692,6 +692,7 @@ def advanced_search(request):
     if min_value.isnumeric() :
         concerts = concerts.filter(Q(price_min__gte=min_value))
 
+    concerts = concerts.distinct()
     try:
         serializer = ConcertSerializer(concerts,many=True)
         return Response(serializer.data, status = status.HTTP_200_OK)
@@ -1104,7 +1105,7 @@ def get_recommendations(request):
             artistIDs.append(item['id'])
     recommendableconcertsfromspotify = Concert.objects.all()
     recommendableconcertsfromspotify = recommendableconcertsfromspotify.filter(Q(artist__spotify_id__in=artistIDs))
-    
+
     if len(recommendableconcertsfromspotify) is not 0 :
         recommendableconcertsfromspotify = recommendableconcertsfromspotify.distinct()
         for concert in recommendableconcertsfromspotify :
@@ -1118,7 +1119,7 @@ def get_recommendations(request):
     if len(recommendableconcertsfromspotify) is not 0 :
         recommendableconcertsfromspotify.distinct()
         recommendedConcerts = recommendedConcerts.union(recommendableconcertsfromspotify, all=True)
-    
+
     #Sort the recomm values
     sorted_countDict = sorted(countDict.items(), key=operator.itemgetter(1), reverse=True)
     #print(sorted_countDict)
@@ -1129,7 +1130,7 @@ def get_recommendations(request):
     concerts = []
     for concert in sorted_countDict:
         #print(concert[0])
-        for l in listRecc : 
+        for l in listRecc :
             if l.concert_id == int(concert[0]):
                 #print(l.concert_id)
                 concerts.append(ConcertSerializer(l).data)
@@ -1147,7 +1148,7 @@ def get_recommendation_by_followed_users(request):
         return Response({'error':'The user needs to sign in first.'}, status = status.HTTP_401_UNAUTHORIZED)
 
     subscribed_concerts = request.user.concerts.all()
-    
+
 
     serializer = ConcertSerializer(recommended_concerts, many = True)
 
@@ -1203,7 +1204,7 @@ USER REPORT FUNCTIONS
 '''
 
 @api_view(['POST','PUT'])
-def create_or_edit_user_report(request,reported_user_id):   
+def create_or_edit_user_report(request,reported_user_id):
     '''
        Creates or edits a user report for the user with the given reported_user_id
     '''
